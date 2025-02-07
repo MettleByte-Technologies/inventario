c8512443d3dbb6e88bea6d71cb1060615b4f3b8e
Jigar <js10855@gmail.com>
2025-02-07 19:04:58 +0530

    CONVERT - TO /

--- js/order-view.js
+++ js/order-view.js
@@ -230,7 +230,7 @@ if($('#sessrolid').val() != 2){
                         let mm = String(date.getMonth() + 1).padStart(2, "0"); // Month is 0-based
                         let dd = String(date.getDate()).padStart(2, "0");
                         let yyyy = date.getFullYear();
-                        return `${mm}-${dd}-${yyyy}`;
+                        return `${mm}/${dd}/${yyyy}`;
                     }
 
                       if (objResulSet["error"] === 0) {
